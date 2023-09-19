8fef9b40debf5ab57aeaa1bf66e86000c1eeb1dd
BYREDDI RAMANA BABU <127107319+BYREDDI@users.noreply.github.com>
2023-09-19 14:49:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function myFunction()
 		let reqele=btnele.previousElementSibling;
 		reqele.remove();
 		let h1ele=document.createElement('h1');
-		h1ele.innerHTML="Entered Metaverse";
+		h1ele.innerText="Entered Metaverse";
 		h1ele.id="status";
 		let bodyele=document.getElementsByTagName("body")[0];
 		bodyele.insertBefore(h1ele,btnele);
